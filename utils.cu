b8689b1abfc0e2aaff755d38c712d9b699ab6754
Svadha Dey <145038630+SvadhaD@users.noreply.github.com>
2025-04-04 01:18:59 +0530

    Update utils.cu

--- utils.cu
+++ utils.cu
@@ -1,15 +1,3 @@
 #include "utils.cuh"
 
-void* cudaMallocSafe(size_t size) {
-    void* devPtr;
-    checkCudaError(cudaMalloc(&devPtr, size), "cudaMalloc failed");
-    return devPtr;
-}
-
-void cudaMemcpySafe(void* dst, const void* src, size_t size, cudaMemcpyKind kind) {
-    checkCudaError(cudaMemcpy(dst, src, size, kind), "cudaMemcpy failed");
-}
-
-void cudaFreeSafe(void* devPtr) {
-    checkCudaError(cudaFree(devPtr), "cudaFree failed");
-}
+// All definitions in utils.cuh
