f72c45e2aec52f696cb44de95d6962809dfbfb71
Svadha Dey <145038630+SvadhaD@users.noreply.github.com>
2025-04-04 02:35:31 +0530

    Update morphology.cuh

--- morphology.cuh
+++ morphology.cuh
@@ -1,25 +1,14 @@
 #ifndef MORPHOLOGY_CUH
 #define MORPHOLOGY_CUH
 
-#include <cuda_runtime.h>
-#include "utils.cuh"
+#include <opencv2/opencv.hpp>
+#include <vector>
 
-// Kernel for dilation operation
-__global__ void dilationKernel(const unsigned char* inputImage, unsigned char* outputImage, 
-                               const int* structuringElement, int imageWidth, int imageHeight, 
-                               int elementWidth);
+void cpu_erosion(const cv::Mat& inputImage, cv::Mat& outputImage, const std::vector<int>& kernel);
+void cpu_dilation(const cv::Mat& inputImage, cv::Mat& outputImage, const std::vector<int>& kernel);
 
-// Kernel for erosion operation
-__global__ void erosionKernel(const unsigned char* inputImage, unsigned char* outputImage, 
-                              const int* structuringElement, int imageWidth, int imageHeight, 
-                              int elementWidth);
-
-// Host function to apply dilation
-void applyDilation(const unsigned char* h_inputImage, unsigned char* h_outputImage, 
-                   const int* h_structuringElement, int imageWidth, int imageHeight, int elementWidth);
-
-// Host function to apply erosion
-void applyErosion(const unsigned char* h_inputImage, unsigned char* h_outputImage, 
-                  const int* h_structuringElement, int imageWidth, int imageHeight, int elementWidth);
+void gpu_erosion(const uchar* input, uchar* output, int width, int height, const int* kernel, int kernelSize);
+void gpu_dilation(const uchar* input, uchar* output, int width, int height, const int* kernel, int kernelSize);
 
 #endif
+
